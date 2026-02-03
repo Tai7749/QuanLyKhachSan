ce9538b138b0ddfa31f40f6854ea71076a21b86d
VanTai <nguyenthieny820@gmail.com>
2026-02-03 16:23:29 +0700

    Them WinForms project

---
+++ QuanLyKhachSan/QuanLyKhachSan.WinForms/DAL/DbHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace QuanLyKhachSan.WinForms.DAL
+{
+    internal class DbHelper
+    {
+        private static string connStr = ConfigurationManager.ConnectionStrings["HotelDb"].ConnectionString;
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connStr);
+        }
+    }
+}
