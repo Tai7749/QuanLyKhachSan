88fbca4ff0354312020c5d2a994d0fba2b9831c2
Phạm Tấn Lợi <tanloipham365@gmail.com>
2026-02-25 23:27:22 +0700

    Add files via upload
    ver w1.0.0

---
+++ Forms/FmLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan.WinForms
+{
+    public partial class FmLogin : Form
+    {
+        public FmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormdangNhap_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_quenmatkhau_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
