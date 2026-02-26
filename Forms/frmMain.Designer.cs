46618c9b911d060842e2533030097dc83501a763
VanTai <nguyenthieny820@gmail.com>
2026-02-26 19:42:48 +0700

    Add button test

--- Forms/frmMain.Designer.cs
+++ Forms/frmMain.Designer.cs
@@ -29,25 +29,38 @@
         private void InitializeComponent()
         {
             this.btnBoPhan = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnBoPhan
             // 
-            this.btnBoPhan.Location = new System.Drawing.Point(12, 82);
+            this.btnBoPhan.Location = new System.Drawing.Point(16, 101);
+            this.btnBoPhan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBoPhan.Name = "btnBoPhan";
             this.btnBoPhan.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.btnBoPhan.Size = new System.Drawing.Size(148, 72);
+            this.btnBoPhan.Size = new System.Drawing.Size(197, 89);
             this.btnBoPhan.TabIndex = 0;
             this.btnBoPhan.Text = "Bộ Phận";
             this.btnBoPhan.UseVisualStyleBackColor = true;
             this.btnBoPhan.Click += new System.EventHandler(this.btnBoPhan_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(662, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.ClientSize = new System.Drawing.Size(1579, 690);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnBoPhan);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Trang chính";
@@ -58,5 +71,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnBoPhan;
+        private System.Windows.Forms.Button button1;
     }
 }
